cc53a7038907b539744cfa5153b4b176fa74dd1e
Srikanth Alla <sreekanthalla16@gmail.com>
2023-09-30 06:43:02 +0530

    Created a landing page with 3 buttons and logo

--- venv/Main.py
+++ venv/Main.py
@@ -1,16 +1,17 @@
+import os
+import shutil
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QPushButton, QLabel, QFileDialog, QListWidget, QListWidgetItem, QGridLayout, QScrollArea
-from PyQt5.QtGui import QPixmap
-from PyQt5 import QtCore
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QPushButton, QLabel, QFileDialog, QGridLayout, QScrollArea
+from PyQt5.QtGui import QPixmap, QImage
 from PIL import Image, ImageQt
+from PyQt5 import QtCore
+from PyQt5.QtCore import QSize
 import io
-
 class SmartSelect(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initui()
         self.photos = []  # List to store photo data
-
     def initui(self):
         self.setWindowTitle('Smart Select Photo Album App')
         self.setGeometry(100, 100, 800, 600)
@@ -25,23 +26,67 @@ class SmartSelect(QMainWindow):
         header_label.setStyleSheet('font-size: 24px; font-weight: bold;')
         layout.addWidget(header_label)
 
-        self.photo_scroll_area = QScrollArea(self)
-        layout.addWidget(self.photo_scroll_area)
+        logo_label = QLabel(self)
+        logo_pixmap = QPixmap('/Users/srialla/SmartSelect/venv/Media/Entry.png')
+        max_logo_size = QSize(800,600)
+        logo_pixmap = logo_pixmap.scaled(max_logo_size, QtCore.Qt.KeepAspectRatio)
+        logo_label.setPixmap(logo_pixmap)
+        logo_label.setAlignment(QtCore.Qt.AlignCenter)
+        layout.addWidget(logo_label)
 
-        self.photo_scroll_content = QWidget(self)
-        self.photo_scroll_area.setWidget(self.photo_scroll_content)
+        # self.photo_scroll_area = QScrollArea(self)
+        # layout.addWidget(self.photo_scroll_area)
+        #
+        # self.photo_scroll_content = QWidget(self)
+        # self.photo_scroll_area.setWidget(self.photo_scroll_content)
+        #
+        # self.photo_grid_layout = QGridLayout(self.photo_scroll_content)
+        # self.photo_grid_layout.setAlignment(QtCore.Qt.AlignTop)
 
-        self.photo_grid_layout = QGridLayout(self.photo_scroll_content)
-        self.photo_grid_layout.setAlignment(QtCore.Qt.AlignTop)
+        upload_button = QPushButton('Design Album', self)
+        help_button = QPushButton('Help', self)
+        settings_button = QPushButton('Settings', self)
 
-        upload_button = QPushButton('Upload Photos', self)
-        create_album_button = QPushButton('Create Album', self)
-
-        upload_button.clicked.connect(self.uploadPhotos)
-        create_album_button.clicked.connect(self.createAlbum)
+        upload_button.clicked.connect(self.design_album)
+        help_button.clicked.connect(self.help)
+        settings_button.clicked.connect(self.settings)
 
         layout.addWidget(upload_button)
-        layout.addWidget(create_album_button)
+        layout.addWidget(help_button)
+        layout.addWidget(settings_button)
+    def design_album(self):
+       album_name, ok = QInputDialog.getText(self, 'Create Album', 'Enter album name:')
+
+       if ok and album_name:
+            album_dir = f'./{album_name}'  # You can change the directory path as needed
+            try:
+                os.makedirs(album_dir)
+                QMessageBox.information(self, 'Success', f'Album "{album_name}" created successfully!')
+            except OSError as e:
+                QMessageBox.critical(self, 'Error', f'Failed to create album directory: {str(e)}')
+
+            # Prompt the user to upload photos to the album directory
+            options = QFileDialog.Options()
+            options |= QFileDialog.ReadOnly
+            file_paths, _ = QFileDialog.getOpenFileNames(self, f'Upload Photos to {album_name}', '', 'Images (*.png *.jpg *.jpeg *.gif *.bmp);;All Files (*)', options=options)
+
+            if file_paths:
+                for file_path in file_paths:
+                    try:
+                        # Copy the selected photos to the album directory
+                        shutil.copy(file_path, album_dir)
+                    except Exception as e:
+                        print(f"Error copying file: {str(e)}")
+            else:
+                QMessageBox.information(self, 'Info', 'No photos selected for upload.')
+
+    def help(self):
+        # Replace this with the code to open the Help page
+        pass
+
+    def settings(self):
+        # Replace this with the code to open the Settings page
+        pass
 
     def uploadPhotos(self):
         options = QFileDialog.Options()
@@ -53,90 +98,44 @@ class SmartSelect(QMainWindow):
                 with open(file_path, 'rb') as file:
                     photo_data = file.read()
 
-            if self.isValidImage(photo_data):
-                compressed_photo_data = self.compressImage(photo_data)
-                if compressed_photo_data:
-                    self.photos.append(compressed_photo_data)
-                    self.displayPhoto(compressed_photo_data)
-            else:
-                print(f"Error: Invalid image file: {file_path}")
+                if self.isValidImage(photo_data):
+                    compressed_photo_data = self.compressImage(photo_data)
+                    if compressed_photo_data:
+                        self.photos.append(compressed_photo_data)
+                        self.displayPhoto(compressed_photo_data)
+                else:
+                    print(f"Error: Invalid image file: {file_path}")
 
     def isValidImage(self, data):
         try:
-            image = Image.open(io.BytesIO(data))
-
+            Image.open(io.BytesIO(data))
             # Check if the image format is recognized (valid image)
             return True
         except Exception as e:
             print(f"Error: {e}")
             return False
 
-
     def compressImage(self, photo_data, quality=75):
-        # Create a PIL Image from the photo data
-        image = Image.open(io.BytesIO(photo_data))
-
-        # Compress the image using the specified quality (0-100)
         try:
+            image = Image.open(io.BytesIO(photo_data))
             image = image.convert("RGB")  # Ensure the image is in RGB mode
             image = image.resize((300, 200), Image.ANTIALIAS)  # Resize for display (adjust dimensions as needed)
             output = io.BytesIO()
             image.save(output, format="JPEG", quality=quality)
-
-
             return output.getvalue()
-
         except Exception as e:
-            print(f"failed to compress the image:{e}")
+            print(f"Failed to compress the image: {e}")
             return None
 
     def displayPhoto(self, photo_data):
         pixmap = QPixmap()
-        image = ImageQt(Image.open(io.BytesIO(photo_data)))
+        image = ImageQt.ImageQt(Image.open(io.BytesIO(photo_data)))
         pixmap.convertFromImage(image)
         label = QLabel(self)
         label.setPixmap(pixmap)
 
         row, col = divmod(len(self.photos) - 1, 3)
         self.photo_grid_layout.addWidget(label, row, col)
-
-    def createAlbum(self):
-        album_window = AlbumWindow(self)
-        album_window.show()
-
-class AlbumWindow(QWidget):
-    def __init__(self, parent):
-        super().__init__()
-        self.parent = parents
-        self.albums = {}
-        self.initui()
-
-    def initui(self):
-        self.setWindowTitle('Album Management')
-        self.setGeometry(200, 200, 400, 300)
-
-        layout = QVBoxLayout(self)
-
-        self.album_list = QListWidget(self)
-        layout.addWidget(self.album_list)
-
-        add_photos_button = QPushButton('Add Photos to Album', self)
-        create_album_button = QPushButton('Create Album', self)
-
-        add_photos_button.clicked.connect(self.addPhotosToAlbum)
-        create_album_button.clicked.connect(self.createAlbum)
-
-        layout.addWidget(add_photos_button)
-        layout.addWidget(create_album_button)
-
-    def createAlbum(self):
-        album_name, ok = QInputDialog.getText(self, 'Create Album', 'Enter album name:')
-        if ok and album_name:
-            self.albums[album_name] = []
-            self.album_list.addItem(album_name)
-
-    def addPhotosToAlbum(self):
-        pass
 
 def main():
     app = QApplication(sys.argv)
